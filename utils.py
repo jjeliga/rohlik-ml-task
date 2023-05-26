fcdf9d467e5987059ff1ac51cb9939dc6d15fd51
jan_jeliga <jan.jeliga@datasentics.com>
2023-05-26 13:01:31 -0700

    utils for use in eda and further modelling

---
+++ utils.py
@@ -0,0 +1,84 @@
+import pandas as pd
+
+def map_ids(df: pd.DataFrame, id_col: str):
+    """
+    Maps values of id_col to string sequence '0', '1', ...
+    Serves only for better readability.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe.
+    id_col : str
+        Column on which to perform mapping.
+
+    Returns
+    -------
+    Transformed df and mapping.
+
+    """
+    
+    old_ids = set(df[id_col])
+    ids = [str(i) for i in range(len(old_ids))]
+    id_map = dict(zip(old_ids, ids))
+    
+    df[id_col] = df[id_col].map(id_map)
+    
+    return df, id_map
+
+def split_df(df: pd.DataFrame, id_col: str) -> dict:
+    """
+    Splits dataframe to several smaller dataframes by a selected id column.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to be splited to several smaller dataframes.
+    id_col : str
+        Column to split by.
+
+    Returns
+    -------
+    dict
+        Dict of id and corresponding dataframe.
+
+    """        
+    pids = sorted(list(set(df[id_col])))
+    pid_df = {pid : df[df[id_col] == pid] for pid in pids}
+
+    return pid_df   
+
+
+def transform_df(
+        df: pd.DataFrame, sort_cols: list, col_pc_td: dict
+    ) -> pd.DataFrame:
+    """
+    Transforms a datframe of sales and prices data.
+    Currently only sorting and pct_change is implemented
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DESCRIPTION.
+    sort_cols: list
+        Columns to sort by.
+    col_pc_td : dict
+        Mapping of columns and time window sizes for pct_change.
+        Sample input: {"sell_price": [1]}
+
+    Returns
+    -------
+    pd.DataFrame
+        Transformed dataframe.
+
+    """
+    df = df.sort_values(sort_cols)
+    
+    for pcc, tdeltas in col_pc_td.items():
+        if pcc in df.columns:
+            for td in tdeltas:
+                df[f"{pcc}_pct_change_{td}"] = df[pcc].pct_change(td)
+                
+    return df
+
+
