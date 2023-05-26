22070056a0eb123ac5147bbd4b52f9f962b8ac13
jan_jeliga <jan.jeliga@datasentics.com>
2023-05-26 11:21:54 -0700

    small modifications

--- eda.py
+++ eda.py
@@ -15,14 +15,25 @@ import os
 
 pd.set_option('display.max_columns', None)
 
-os.chdir("c:\\Text\\work_search_summer23\\rohlik\\task")
+# if __name__=="__main__":
+#     print(os.getcwd())
+#     print(os.path.dirname(os.path.realpath(__file__)))
+
+
+os.chdir("c:\\Text\\work_search_summer23\\rohlik\\ml_task")
 df_prices = pd.read_csv("ml_task_data.csv")
 
+# %%
 # basic info
-df_prices.head()
+print(df_prices.head())
+
+# %%
 df_prices.info() # check NAs
+
+# %%
 df_prices.describe()
 
+# %%
 # convert date col to datetime format
 df_prices.date = pd.to_datetime(df_prices.date, format="%Y-%m-%d")
 
@@ -32,26 +43,46 @@ pids = [str(i) for i in range(len(old_pids))]
 id_map = dict(zip(old_pids, pids))
 
 df_prices.product_id = df_prices.product_id.map(id_map)
-df_prices.head()
 
-# sort for better navigation, cheap since the data is small
-df_prices = df_prices.sort_values(["product_id","date"])
+# %% split to individual dfs, necessary for correct pct_change calculation
+pid_df = {}
+for pid in pids:
+    pid_df[pid] = (
+        df_prices
+        .query(f"product_id == '{pid}'")
+        .sort_values("date")
+        )
 
+
+# %% add percent price change
+
+for pid in pids:
+    pid_df[pid]["sell_price_pct_change"] = pid_df[pid]["sell_price"].pct_change(1)
+
+
+# %%
 # check basic statistics per product
-for col in ["sell_price", "margin", "sales"]:
-    print(df_prices.groupby("product_id")[[col]].agg(["min", "median", "mean", "max", "count", "std"]))
+cols = ["sell_price", "margin", "sales", "sell_price_pct_change"]
 
+for pid, df in pid_df.items():
+    print(pid)
+    print(df[cols].agg(["min", "median", "mean", "max", "count", "std"]))
+
+
+# %%
 # check dates span
 df_dates_ranges = df_prices.groupby("product_id")[["date"]].agg(["min", "max"])
 print(df_dates_ranges)
 
+# %%
 missing_dates = {}
-for pid in pids:
-    df_pid = df_prices[df_prices.product_id == pid]
+for pid, df in pid_df.items():
     dmin, dmax = df_dates_ranges.loc["0"]
-    missing_dates[pid] = pd.date_range(start = dmin, end = dmax).difference(df_pid.date)
+    missing_dates[pid] = pd.date_range(start = dmin, end = dmax).difference(df.date)
     
 print(missing_dates)
+
+# %%
 # visualisation of missing dates
 for pid, missing in missing_dates.items():
     dmin, dmax = df_dates_ranges.loc[pid]
@@ -69,15 +100,27 @@ for pid, missing in missing_dates.items():
 # we can try dropping or performing ffill/interpolation and then compare, 
 # the thing is, that this might be basically equivalent
 
-# there is a visible spike in sales after a price drop, otherwise some noise + trend
-for pid in pids:
-    df_pid = df_prices[df_prices.product_id == pid]
-    plt.plot(df_pid.date, df_pid.sell_price, df_pid.date, df_pid.sales, df_pid.date, df_pid.margin, linewidth=1)
-    plt.title(f"price, sales and margin of product {pid}")
-    plt.legend(["sell_price", "sales", "margin"])
+# %% exploring relation of sales and other varibales
+# there is a visible spike in sales after a price drop which lasts 
+# -> create a sale indicator
+# otherwise some noise + trend
+
+for pid, df in pid_df.items():
+    # only for visualisation purposes of price changes, no interpretation
+    df["pct_change_scaled"] = (df["sell_price_pct_change"] * 
+        df["sell_price"].min() / df["sell_price_pct_change"].max())
+    # creating and saving the plot of sales and other variables
+    plt.plot(
+        df.date, df.sell_price,
+        df.date, df.sales,
+        df.date, df.margin,
+        df.date, df.pct_change_scaled,
+        linewidth=1)
+    plt.title(f"sales and related variables of product {pid}")
+    plt.legend(["sell_price", "sales", "margin", "pct_change_scaled"])
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig(f"{pid}_price_sales_margin.png", dpi=500)
+    plt.savefig(f"{pid}_sales_related.png", dpi=500)
     plt.close()
 
 
