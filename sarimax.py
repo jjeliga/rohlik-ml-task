ea19fe1332a6a458854d85997844fd4f31a47fd9
jan_jeliga <jan.jeliga@datasentics.com>
2023-05-28 15:46:11 -0700

    visualisations of results v0

--- sarimax.py
+++ sarimax.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error, mean_squared_error
 from statsforecast import StatsForecast
 from statsforecast.models import AutoARIMA
@@ -31,14 +32,12 @@ pid = "0"
 df = pid_df[pid]
 # df.set_index(pd.DatetimeIndex(df[DATE_COL]), drop=False, inplace=True)
 
-dff = df[[ID_COL, DATE_COL, SALES_COL]]
-dff.columns = ["unique_id", "ds", "y"]
-dff_train = dff[:-14]
-dff_test =  dff[-14:]  # take last 2 weeks of data
+exog_cols = [f"{PRICE_COL}_pct_change_1"]
 
-# %%
-df_train = df_train.astype({SALES_COL: np.float64})
-df_train.dtypes
+dff = df[[ID_COL, DATE_COL, SALES_COL, *exog_cols]]
+dff.columns = ["unique_id", "ds", "y", *exog_cols]
+dff_train = dff[1:-14]
+dff_test =  dff[-14:]  # take last 2 weeks of data
 
 
 # %%
@@ -47,9 +46,16 @@ df_train.dtypes
 
 sf = StatsForecast(
     models = [AutoARIMA(
-        season_length = 7
+        max_q=5,
+        max_p=5,
+        max_d=3,
+        max_Q=3,
+        max_P=3,
+        max_D=1,
+        season_length=7
         )],
-    freq = 'D'
+    freq='D',
+    # n_jobs=-1
 )
 
 sf.fit(dff_train)
@@ -57,7 +63,9 @@ model_info = sf.fitted_[0][0].model_
 
 # %%
 
-forecast_df = sf.predict(h=14, level=[95]) 
+X_df = dff_test.drop(["y"], 1) if exog_cols else None
+
+forecast_df = sf.predict(h=14, X_df=X_df, level=[95]) 
 dff_test_pred = dff_test.merge(forecast_df, on=["unique_id", "ds"])
 dff_test_pred
 
@@ -68,3 +76,28 @@ RMSE = np.sqrt(mean_squared_error(dff_test_pred.y, dff_test_pred.AutoARIMA))
 print("MAE:  ", round(MAE, 2))
 print("MAPE: ", round(MAPE, 2), "%")
 print("RMSE: ", round(RMSE, 2))
+
+
+
+# %%
+
+
+dff_test_pred.set_index("ds", inplace=True)
+dff_test_pred[['y', "AutoARIMA"]].plot()
+
+# %%
+plot_cols = ["y", "ds", "AutoARIMA", "AutoARIMA-lo-95", "AutoARIMA-hi-95"]
+
+f_df = sf.forecast(h=14, X_df=X_df, fitted=True, level=[95]) 
+insample_fcsts_df = sf.forecast_fitted_values()
+
+df_res_vis = pd.concat([insample_fcsts_df[plot_cols], dff_test_pred[plot_cols]])
+df_res_vis.set_index("ds", inplace=True)
+
+df_res_vis.plot(linewidth=.5)
+plt.axvline(x=insample_fcsts_df.ds[-1], linewidth=.3)
+plt.savefig("test_predict.png", dpi=500)
+
+
+
+
